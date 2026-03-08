2d7966b43f2ac2e31f7fc771223045ffda91830a
idjukic102 <idjukic10223rn@raf.rs>
2026-03-08 23:54:02 +0100

    first commit

---
+++ banka2_bek/src/main/java/rs/raf/banka2_bek/Banka2BekApplication.java
@@ -0,0 +1,13 @@
+package rs.raf.banka2_bek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Banka2BekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Banka2BekApplication.class, args);
+	}
+
+}
---
+++ banka2_bek/src/test/java/rs/raf/banka2_bek/Banka2BekApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.raf.banka2_bek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Banka2BekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
